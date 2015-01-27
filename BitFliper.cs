3c1d0faad5891b963f50f8a1108861cab04633ae
Georgi Krystev <george.krystew@gmail.com>
2015-01-27 17:56:59 +0200

    Flipping every three equal bits to opposite bit number

---
+++ BitFliper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+     class FirstBitFliper
+    {
+        static void Main()
+        {
+            //ulong num = ulong.Parse(Console.ReadLine());
+            long num =  8773276988229695713;
+            string numAsString = Convert.ToString(num, 2).PadLeft(64,'0');
+            char[] numAsCharArry = numAsString.ToCharArray();
+
+            for (int i = 2; i < numAsString.Length; i++)
+            {
+                if (numAsCharArry[i] == numAsCharArry[i - 1] && numAsCharArry[i] == numAsCharArry[i - 2])
+                {
+                    if (numAsCharArry[i] == '0')
+                    {
+                        numAsCharArry[i] = '1';
+                        numAsCharArry[i - 1] = '1';
+                        numAsCharArry[i - 2] = '1';
+                    }
+                    else
+                    {
+                        numAsCharArry[i] = '0';
+                        numAsCharArry[i - 1] = '0';
+                        numAsCharArry[i - 2] = '0';
+                    }
+                    i += 2;
+                    //Console.WriteLine(num);
+                    //Console.WriteLine(numAsCharArry[i]);                                 
+ 
+                }
+                
+            }
+            Console.WriteLine(num);
+            Console.WriteLine(numAsCharArry);
+
+
+        }
+    }
+
